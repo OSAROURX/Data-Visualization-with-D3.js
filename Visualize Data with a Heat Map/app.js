66d583abe897b855f8285f0969daae5825dab533
LeviaThanSr <omarsarour@gmail.com>
2021-05-27 18:34:44 +0100

    Update App

--- Visualize Data with a Heat Map/app.js
+++ Visualize Data with a Heat Map/app.js
@@ -1,1 +1,35 @@
-console.log("Hello World!");
+const url =
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
+const height = 550;
+const width = 950;
+const padding = 60;
+
+let xScale;
+let yScale;
+let dataset;
+
+const container = d3
+  .select("body")
+  .append("div")
+  .attr("class", "container")
+  .style("position", "relative");
+
+const svg = d3
+  .select(".container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .attr("class", "svg");
+
+const tooltip = d3
+  .select(".container")
+  .append("div")
+  .attr("id", "tooltip")
+  .style("visibility", "hidden");
+
+fetch(url)
+  .then((response) => response.json())
+  .then((response) => {
+    dataset = response;
+    console.log(dataset);
+  });
